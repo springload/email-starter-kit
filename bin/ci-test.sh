5a97a6ded01f612e3948604233b584b928073a5e
Thibaud Colas <thibaudcolas@gmail.com>
2017-08-31 11:22:39 +0300

    Add basic link checking tests with hyperlink

---
+++ bin/ci-test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Test script for the project. To be ran on each build within a CI environment.
+
+# Fail on first line that fails.
+set -e
+
+# Link checking
+hyperlink templates/main-festival/index.html
+hyperlink templates/article-magazine/index.html
+hyperlink templates/culture-club/index.html --exclude https://twitter.com/CultureClubNZ
+hyperlink templates/culture-club-auto-response/index.html --exclude https://twitter.com/CultureClubNZ
+
+exit 0
